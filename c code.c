108f42e3475dfd1cbf763726b33940e22d6743d5
musasizibrian <118959762+musasizibrian@users.noreply.github.com>
2024-09-01 21:23:09 +0300

    Update c code.c
    C/C++ program to read command line arguments and just print them.
    added comments

--- c code.c
+++ c code.c
@@ -1,7 +1,9 @@
-
+//C program to read command line arguments and just print them.
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+ // argc: Number of arguments, including the program name itself
+    // argv: Array of strings, each string representing an argument
 
 
     for (int i = 0; i < argc; i++) {
